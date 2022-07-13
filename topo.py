9ad591de8d499c7e7a609b77eb275d036d27775d
BlackLutos <gcobs172250@gmail.com>
2022-07-13 05:35:02 +0000

    update topo.py

--- topo.py
+++ topo.py
@@ -11,26 +11,26 @@ from mininet.term import makeTerm
 from mininet.node import OVSSwitch
 
 class design_Topo(Topo):
-        def __init__(self,**opts):
-                Topo.__init__(self,**opts)
-                h1 = self.addHost('h1',mac = '00:00:00:00:00:01')
-                h2 = self.addHost('h2',mac = '00:00:00:00:00:02')
+	def __init__(self,**opts):
+		Topo.__init__(self,**opts)
+		h1 = self.addHost('h1',mac = '00:00:00:00:00:01')
+		h2 = self.addHost('h2',mac = '00:00:00:00:00:02')
 		s1 = self.addSwitch('s1')
-                s2 = self.addSwitch('s2')
-                s3 = self.addSwitch('s3')
-                self.addLink(h1,s1)
-                self.addLink(h2,s2)
+		s2 = self.addSwitch('s2')
+		s3 = self.addSwitch('s3')
+		self.addLink(h1,s1)
+		self.addLink(h2,s2)
 		self.addLink(s1,s2)
-                self.addLink(s2,s3)
-                self.addLink(s3,s1)
+		self.addLink(s2,s3)
+		self.addLink(s3,s1)
 def topo_start():
-        topo = design_Topo()
-        net = Mininet(topo=topo,controller = RemoteController,link = TCLink)
-        net.start()
-        CLI(net)
-        # insert automatically 
-        net.stop()
+	topo = design_Topo()
+	net = Mininet(topo=topo,controller = RemoteController,link = TCLink)
+	net.start()
+	CLI(net)
+	# insert automatically 
+	net.stop()
 if __name__=='__main__':
-        #setLogLevel('info')
-        topo_start()
+	#setLogLevel('info')
+	topo_start()
 
