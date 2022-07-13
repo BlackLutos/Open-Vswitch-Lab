cf5ca0d5df1c888f71f0ed56f40069bb0899902c
BlackLutos <gcobs172250@gmail.com>
2022-07-13 09:31:56 +0000

    update

--- topo.py
+++ topo.py
@@ -15,8 +15,12 @@ from mininet.node import OVSSwitch
 class design_Topo(Topo):
 	def __init__(self,**opts):
 		Topo.__init__(self,**opts)
-		h1 = self.addHost('h1',mac = '00:00:00:00:00:01')
-		h2 = self.addHost('h2',mac = '00:00:00:00:00:02')
+		# h1 = self.addHost('h1',mac = '00:00:00:00:00:01')
+		# h2 = self.addHost('h2',mac = '00:00:00:00:00:02')
+		# h1 = self.addHost('h1', ip="192.168.15.2/24")
+		# h2 = self.addHost('h2', ip="192.168.15.3/24")
+		h1 = self.addHost('h1')
+		h2 = self.addHost('h2')
 		s1 = self.addSwitch('s1')
 		s2 = self.addSwitch('s2')
 		s3 = self.addSwitch('s3')
@@ -25,13 +29,17 @@ class design_Topo(Topo):
 		self.addLink(s1,s2)
 		self.addLink(s2,s3)
 		self.addLink(s3,s1)
+		# self.cmd('ifconfig h1-eth0 192.168.15.2/24 up')
 def topo_start():
 	topo = design_Topo()
 	net = Mininet(topo=topo,controller = RemoteController,link = TCLink)
 	net.start()
-	sh.bash("topo_flow.sh")
-	CLI(net)
+	# sh.bash("topo_flow.sh")
 	# insert automatically
+	hosts = net.hosts
+	hosts[0].cmd('ifconfig h1-eth0 192.168.15.2/24 up')
+	# print(hosts)
+	CLI(net)
 	net.stop()
 if __name__=='__main__':
 	#setLogLevel('info')
