ee2149b3bbc5daa9693eba522a3b275dde27616c
BlackLutos <gcobs172250@gmail.com>
2022-07-26 09:42:11 +0000

    update

--- mininet_to_internet/script.py
+++ mininet_to_internet/script.py
@@ -19,18 +19,17 @@ def topo_start():
     hosts = net.hosts
     s1 = switch[0]
     h1 = hosts[0]
-    s1.cmd('ifconfig s1 192.168.0.1/24') 
-    s1.cmd('sysctl -w net.ipv4.ip_forward=1') # 開啟封包傳遞
-    s1.cmd('sysctl -p') # 立刻讓該設定生效
+    s1.cmd('ifconfig s1 192.168.0.1/24') # Configure IP Address to S1
+    s1.cmd('sysctl -w net.ipv4.ip_forward=1') # Open IP forwarding
+    s1.cmd('sysctl -p') # Make configuration become effective instantly
     s1.cmd('iptables -t nat -A POSTROUTING -s 192.168.0.1/24 -o ens33 -j MASQUERADE')
-    # -t table -A append -o out-interface
     s1.cmd('cp ./isc-dhcp-server /etc/default')
     s1.cmd('cp ./dhcpd.conf /etc/dhcp')
     s1.cmd('sudo systemctl restart isc-dhcp-server')
     time.sleep(5)
-    h1.cmd("udhcpc -i h1-eth0") #自動獲取IP
+    h1.cmd("udhcpc -i h1-eth0") # To configure ip by DHCP Server 
     h1.cmd("ping -c 4 8.8.8.8 > ping_result.log")
-    CLI(net)
+    #CLI(net)
     net.stop()
 if __name__=='__main__':
     #setLogLevel('info')
