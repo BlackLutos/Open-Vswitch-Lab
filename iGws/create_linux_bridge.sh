031a0516a99d7621de8e7a69bb391404b3f1b18c
BlackLutos <gcobs172250@gmail.com>
2022-08-24 08:14:08 +0000

    update

--- iGws/create_linux_bridge.sh
+++ iGws/create_linux_bridge.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 brctl addbr br0
+ip netns add h1
 
 ip link add br-eth1 type veth peer name h-eth1
 ip link add br-eth2 type veth peer name h-eth2
 ip link add br-eth3 type veth peer name h-eth3
 
 brctl addif br0 br-eth1 
-brctl addif br0 br-eth2 
-brctl addif br0 br-eth3 
+brctl addif br0 br-eth2     
+brctl addif br0 br-eth3
+
+ip link set h-eth1 netns h1
+
+ifconfig br0 192.168.0.1/24
+ifconfig br-eth1 up
+
+sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+iptables -t nat -A POSTROUTING -s 192.168.0.1/24 -o ens33 -j MASQUERADE
+cp ./isc-dhcp-server /etc/default
+cp ./dhcpd.conf /etc/dhcp
+sudo systemctl restart isc-dhcp-server
+udhcpc -i h-eth1
 
 brctl show
+# ip netns exec h1 /bin/bash --rcfile <(echo "PS1=\"namespace h1> \"")
 exec bash
--- iGws/delete_linux_bridge.sh
+++ iGws/delete_linux_bridge.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+ip link set br0 down
 brctl delbr br0
+ip netns del h1
 brctl show
 exec bash
