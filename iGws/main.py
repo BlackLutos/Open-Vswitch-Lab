4afb47960a088b9a3ee8057b04653c4d261044c3
BlackLutos <gcobs172250@gmail.com>
2022-08-30 09:57:02 +0000

    update

--- iGws/main.py
+++ iGws/main.py
@@ -20,7 +20,7 @@ if __name__=='__main__':
     os.system(del_eth_port)
     # os.system(add_eth_port)
     os.system(ovs_add_port)
-    os.system('ifconfig ovs-br0 0.0.0.0/24 up')
+    os.system('ifconfig br0 0.0.0.0/24')
     os.system('ifconfig ovs-br0 192.168.0.1/24 up')
     os.system('ifconfig ' + eth_port + ' up')
 
@@ -30,7 +30,8 @@ if __name__=='__main__':
     # print(diable_eth_port)
     # os.system('ip link add ' + eth_port + 'type veth peer name ' + h_eth_port)
     # os.system('ovs-vsctl add-port ovs-br0 ' + port)
-    
+    # ip netns exec h1 /bin/bash --rcfile <(echo "PS1=\"namespace h1> \"")
+    # ip netns exec h1 ping google.com -c 3
     # os.system('python3 delete.py')
 
 	
