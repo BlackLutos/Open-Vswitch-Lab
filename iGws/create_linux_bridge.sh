29570987f55747975f63e2350b24ba532a4851c4
BlackLutos <gcobs172250@gmail.com>
2022-09-01 06:05:29 +0000

    udpate

--- iGws/create_linux_bridge.sh
+++ iGws/create_linux_bridge.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-brctl addbr br0
+# brctl addbr br0
+sudo service network-manager start
+sudo nmcli con add ifname br0 type bridge con-name br0
+
 ip netns add h1
 
 ip link add br-eth1 type veth peer name h-eth1
 ip link add br-eth2 type veth peer name h-eth2
 ip link add br-eth3 type veth peer name h-eth3
 
-brctl addif br0 br-eth1 
-brctl addif br0 br-eth2     
-brctl addif br0 br-eth3
+# brctl addif br0 br-eth1 
+# brctl addif br0 br-eth2     
+# brctl addif br0 br-eth3
+nmcli con add type bridge-slave ifname br-eth1 master br0
+nmcli con add type bridge-slave ifname br-eth2 master br0
+nmcli con add type bridge-slave ifname br-eth3 master br0
 
 ip link set h-eth3 netns h1
 
-ifconfig br0 192.168.0.1/24 up
+# ifconfig br0 192.168.0.1/24 up
+sudo nmcli connection modify br0 ipv4.addresses '192.168.0.1/24'
+sudo nmcli con modify br0 ipv4.method manual
+sudo nmcli con up br0 
+
 ifconfig br-eth3 up
 
 sysctl -w net.ipv4.ip_forward=1
@@ -24,7 +34,8 @@ cp ./dhcpd.conf /etc/dhcp
 sudo systemctl restart isc-dhcp-server
 ip netns exec h1 udhcpc -i h-eth3
 
-brctl show
+# brctl show
+nmcli connection show
 
 # ip netns exec h1 /bin/bash udhcpc -i h-eth3
 # ip netns exec h1 /bin/bash --rcfile <(echo "PS1=\"namespace h1> \"")
