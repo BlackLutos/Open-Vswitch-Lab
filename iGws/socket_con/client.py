801c4ae8cb983a89c55ac067bfa5bf29dbbf0159
BlackLutos <gcobs172250@gmail.com>
2022-09-12 07:09:07 +0000

    udpate

--- iGws/socket_con/client.py
+++ iGws/socket_con/client.py
@@ -1,12 +1,20 @@
 import asyncio
 import websockets
 
-async def hello(uri):
+async def hello(uri,data):
     async with websockets.connect(uri) as websocket:
-        await websocket.send("Jimmy")
-        print(f"(client) send to server: Jimmy")
+        await websocket.send(data)
+        print("(client) send to server: " + data)
         name = await websocket.recv()
-        print(f"(client) recv from server {name}")
+        print("(client) recv from server " + name)
+flag = True
+while(flag):
+    n = input()
+    asyncio.get_event_loop().run_until_complete(
+    hello('ws://localhost:8765',n))
+    if n == '0':
+        flag = False
 
-asyncio.get_event_loop().run_until_complete(
-    hello('ws://localhost:8765'))
+
+# asyncio.get_event_loop().run_until_complete(
+#     hello('ws://localhost:8765'))
